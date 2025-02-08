6f79532ead48f5a5032b49303f011b4cc30c31f8
rtkay123 <dev@kanjala.com>
2025-02-08 10:18:49 +0200

    feat: tracing

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# `services`
+# services
 
 This crate helps configure and manage sellershut services using conditional compilation
+
+## Features
+
+- **tracing**: Structured, context-aware logging using [tracing](https://docs.rs/tracing).
+
+## Configuration
+
+To enable any of the features in this crate, add the desired feature flags to your `Cargo.toml` under the `[dependencies]` section.
---
+++ src/tracing.rs
@@ -0,0 +1,49 @@
+use tracing_subscriber::{
+    EnvFilter, Layer, Registry, layer::SubscriberExt, util::SubscriberInitExt,
+};
+
+/// Telemetry handle
+#[allow(missing_debug_implementations)]
+pub struct Tracing {}
+
+impl Tracing {
+    /// Create a new builder
+    pub fn builder() -> TracingBuilder {
+        TracingBuilder::default()
+    }
+}
+
+/// A builder for initialising [tracing] layers
+#[allow(missing_debug_implementations)]
+pub struct TracingBuilder {
+    layer: Vec<Box<dyn Layer<Registry> + Sync + Send>>,
+}
+
+impl Default for TracingBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl TracingBuilder {
+    /// Create a new builder
+    pub fn new() -> Self {
+        let types: Box<dyn Layer<Registry> + Sync + Send> =
+            tracing_subscriber::fmt::layer().boxed();
+        TracingBuilder { layer: vec![types] }
+    }
+
+    /// Initialises tracing
+    pub fn build(self, level: Option<std::sync::Arc<str>>) -> Tracing {
+        let level = if let Some(level) = level {
+            level.to_string()
+        } else {
+            "info".to_string()
+        };
+        tracing_subscriber::registry()
+            .with(self.layer)
+            .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| level.into()))
+            .init();
+        Tracing {}
+    }
+}
